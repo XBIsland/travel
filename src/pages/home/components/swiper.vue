f45c7e169ad989694683ed11c1f2f423af430724
XBIsland <13726043109@139.com>
2018-08-07 14:47:49 +0800

    change

---
+++ src/pages/home/components/swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+          <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1807/66/e5a5cec881702f02.jpg_750x200_67bb5691.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1807/c6/44fce1467be17702.jpg_750x200_406f5fc3.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wapper
+    width: 100%
+    // 或者直接height：xx vw
+    height: 0
+    padding-bottom: 26.6%
+    overflow: hidden
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
